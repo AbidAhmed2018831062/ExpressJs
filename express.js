c8562cee82aa70627e4a33443fee23f9247fc336
Abid <ahmedabid3409@gmail.com>
2022-04-18 16:04:44 +0600

    Express Updated

--- express.js
+++ express.js
@@ -4,9 +4,15 @@ app=express();
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
+app.use(express.static(__dirname+"/public/"));
+
+const router=express.Router();
+
+router.get('/',(req,res)=>{
     console.log(req.body);
     res.end("ok");
 });
 
-app.listen(3000);
+app.listen(3000,()=>{
+    console.log("listening on port 3000")
+});
